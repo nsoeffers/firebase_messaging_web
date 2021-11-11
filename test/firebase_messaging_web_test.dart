05f912de70615d994912f43aa1ca0a1ede7a0fd1
Niels Soeffers <nsoeffers@gmail.com>
2021-11-11 23:22:24 +0100

    First version of milestone page

--- test/firebase_messaging_web_test.dart
+++ test/firebase_messaging_web_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:firebase_messaging_web/firebase_messaging_web.dart';
-
 void main() {
   test('adds one to input values', () {
   });
